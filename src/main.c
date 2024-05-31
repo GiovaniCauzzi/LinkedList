607ba4918b05ecf18340bfbcc23122aa8246a223
Giovani <giovanicauzzi@gmail.com>
2024-05-31 20:06:09 -0300

    Added insert and get nth node of the list functions

--- src/main.c
+++ src/main.c
@@ -9,15 +9,22 @@ typedef struct NODE
     struct NODE *next;
 } NODE;
 
-void print_one_node(NODE * someNode)
+void print_one_node(NODE *someNode)
 {
-    printf("CURRENT NODE       : %x\n", someNode);
-    printf("Pointer to previous: %x\n", someNode->previous);
-    printf("Pointer to next:     %x\n", someNode->next);
-    printf("Data:                %x\n\n", someNode->data);
+    if (someNode != NULL)
+    {
+        printf("CURRENT NODE       : %x\n", someNode);
+        printf("Pointer to previous: %x\n", someNode->previous);
+        printf("Pointer to next:     %x\n", someNode->next);
+        printf("Data:                %x\n\n", someNode->data);
+    }
+    else
+    {
+        printf("----> Print one node: NULL pointer <----\n");
+    }
 }
 
-NODE* find_last_node(NODE *head)
+NODE *find_last_node(NODE *head)
 {
     uint32_t counter = 0;
     if (head == NULL)
@@ -31,31 +38,31 @@ NODE* find_last_node(NODE *head)
         current = current->next;
     }
 
-
     // printf("Last node iterations: %d \n", counter);
-    return  current;
+    return current;
 }
 
-void print_all_nodes(NODE * someNode)
+void print_all_nodes(NODE *someNode)
 {
-    uint32_t counter = 0; 
+    uint32_t counter = 0;
     NODE *node = someNode;
-    
-    do 
+
+    do
     {
         print_one_node(node);
         node = node->next;
         counter++;
-    }while (node->next != NULL) ;
+    } while (node->next != NULL);
+    print_one_node(node);
     printf("Counter: %d \n", counter);
 }
 
-void append_node(NODE * someNode)
+void append_node(NODE *someNode)
 {
     static uint32_t someData = 0;
 
     NODE *last = find_last_node(someNode);
-    NODE *new = (NODE*)malloc(sizeof(NODE));
+    NODE *new = (NODE *)malloc(sizeof(NODE));
 
     last->next = new;
 
@@ -66,18 +73,62 @@ void append_node(NODE * someNode)
     someData++;
 }
 
+NODE* get_nth_node_of_list(NODE *firstNode, uint32_t n)
+{   
+    NODE* auxNode = firstNode;
+
+    for(uint32_t i = 0 ; i < n ; i++)
+    {
+        if(auxNode->next == NULL)
+        {
+            return NULL;
+        }
+        else
+        {
+            auxNode = auxNode->next;
+        }
+    }
+
+    return auxNode;
+}
+
+NODE* insert_new_node_after(NODE *thisNode)
+{
+    NODE *new = (NODE *)malloc(sizeof(NODE));
+    new->data = 0;
+
+    if (thisNode->next != NULL)
+    {
+        thisNode->next->previous = new;
+        new->next = thisNode->next;
+        thisNode->next = new;
+        new->previous = thisNode;
+    }
+    else
+    {
+        thisNode->next = new;
+        new->previous = thisNode;
+    }
+
+    return new;
+}
 
 int main(void)
 {
-    NODE someNode = {0};
+    NODE fistNode = {0};
 
-    someNode.data = 80;
+    fistNode.data = 80;
 
-    for (uint32_t i = 0; i < 25500; i++)
+    for (uint32_t i = 0; i < 10; i++)
     {
-        append_node(&someNode);
+        append_node(&fistNode);
     }
-    print_all_nodes(&someNode);
+    
+    print_all_nodes(&fistNode);
+    printf("\n\n-----------\n\n");
+
+    insert_new_node_after(insert_new_node_after(get_nth_node_of_list(&fistNode, 3)));
+    print_all_nodes(&fistNode);
 
     return 0;
 }
